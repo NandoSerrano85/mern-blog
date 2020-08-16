8b50514f5d75386014bd85f4c7c3135144b592ec
Fernando Serrano <fserr009@fiu.edu>
2020-08-15 20:38:32 -0400

    created model and post route

--- server/Article/router.js
+++ server/Article/router.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router();
+const Article = require('./article.model');
 
-router.post('/', (req, res) =>{
-    
+router.post('/', (req, res) => {
+    Article.create( req.body, (error, data) => {
+        if(error){
+            console.log(`Error creating blog post, ${new Date()}: \n${error}`)
+            res.status(400).json(error);
+        }
+        else{
+            res.status(200).json(data);
+        }
+    })
 })
 
 module.exports = router;
